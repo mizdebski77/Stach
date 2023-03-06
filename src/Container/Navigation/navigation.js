02929f4af9c89dc7225d8fbd76953ef9498678fc
Marcin Izdebski <mizdebski123@gmail.com>
2023-03-06 17:24:18 +0100

    Styled links

--- src/Container/Navigation/navigation.js
+++ src/Container/Navigation/navigation.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { Links, LinksContainer, Logo, Wrapper } from "./styledNavigation";
 
 export const Navigation = () => {
@@ -6,12 +5,15 @@ export const Navigation = () => {
     return (
         <Wrapper>
             <Logo>Mirosław Stach</Logo>
-            <LinksContainer><Links to="/home">Home</Links></LinksContainer>
-            <LinksContainer><Links to="/o-nas">O nas</Links></LinksContainer>
-            <LinksContainer><Links to="/usługi">Usługi</Links></LinksContainer>
-            <LinksContainer><Links to="/realizacje">Realizacjie</Links></LinksContainer>
-            <LinksContainer><Links to="/współpraca">Współpraca</Links></LinksContainer>
-            <LinksContainer><Links to="/kontakt">Kontakt</Links></LinksContainer>
+            <LinksContainer>
+                <Links to="/home">Home</Links>
+                <Links to="/o-nas">O nas</Links>
+                <Links to="/usługi">Usługi</Links>
+                <Links to="/realizacje">Realizacjie</Links>
+                <Links to="/współpraca">Współpraca</Links>
+                <Links to="/kontakt">Kontakt</Links>
+            </LinksContainer>
+
         </Wrapper>
     )
 };
--- src/Container/Navigation/styledNavigation.js
+++ src/Container/Navigation/styledNavigation.js
@@ -2,22 +2,29 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const Wrapper = styled.nav`
-margin: 0;
+margin: 0 ;
 width: 100%;
-height: 100px;
-background: red;
-display: grid;
-grid-template-columns: 1fr 1fr 1fr;
+height: 80px;
+background: wheat;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 20px 100px 20px 100px;
 `;
 
-export const Logo = styled.text`
-height: 100px;
+export const Logo = styled.div`
 font-size: 50px;
 `;
 
 export const LinksContainer = styled.li`
+display: grid;
+grid-template-columns: repeat(6, auto);
+grid-gap: 60px;
+align-items: center;
 
 `;
 export const Links = styled(NavLink)`
 color: black;
+margin: auto;
+
 `;
